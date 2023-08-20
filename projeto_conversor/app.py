5f563150e5d70e0b768322e194031c1267f48083
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-08-20 01:27:42 -0300

    add

--- projeto_conversor/app.py
+++ projeto_conversor/app.py
@@ -1,8 +1,8 @@
 import os
 import threading
-
-from flask import Flask, render_template, request, jsonify, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
+from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
 
 app = Flask(__name__)
@@ -16,39 +16,41 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
+        if 'url' not in request.form:
+            flash('No URL provided')
             return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
+        
+        url = request.form['url']
+        try:
+            youtube = YouTube(url)
+        except:
+            flash('Invalid YouTube URL')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            temp_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(temp_filepath)
-            mp3_filename = os.path.splitext(filename)[0] + '.mp3'
-            mp3_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
 
-            ffmpeg_thread = threading.Thread(target=convert_video_to_mp3, args=(temp_filepath, mp3_filepath))
-            ffmpeg_thread.start()
+        video = youtube.streams.get_highest_resolution()
+        filename = secure_filename(youtube.title + '.mp4')
+        temp_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        video.download(output_path=app.config['UPLOAD_FOLDER'], filename=filename)
 
-            flash('Conversion started, please wait...')
-            return redirect(url_for('index'))
+        mp3_filename = os.path.splitext(filename)[0] + '.mp3'
+        mp3_filepath = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
+
+        ffmpeg_thread = threading.Thread(target=convert_video_to_mp3, args=(temp_filepath, mp3_filepath))
+        ffmpeg_thread.start()
+
+        flash('Conversion started, please wait...')
+        return redirect(url_for('index'))
 
     return render_template('index.html')
 
 def convert_video_to_mp3(temp_filepath, mp3_filepath):
     ffmpeg_extract_audio(temp_filepath, mp3_filepath)
+    os.remove(temp_filepath)  # Remove o arquivo de vídeo temporário após a conversão
 
-@app.route('/check_status', methods=['GET'])
+@app.route('/check_status')
 def check_status():
-    # Implementar a verificação de status da conversão aqui
-    # Retornar o status como um JSON
-    status = {
-        "percent": 100  # Altere para o valor real do progresso
-    }
-    return jsonify(status)
+    # Simular verificação de status (retorne 100% para simular a conclusão da conversão)
+    return jsonify({'percent': 100})
 
 if __name__ == '__main__':
     app.run(debug=True)
